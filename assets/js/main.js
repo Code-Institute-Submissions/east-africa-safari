fc520bc8e1cd9e78bc1570caab3b03fe9cd3099b
jos-lab <josphinemw@gmail.com>
2020-06-26 13:30:20 +0000

    added main.js content, added index.html contentfor filtering added styles for items to places section

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+// items
+const places =[
+    {
+        id:1,
+        title: "maasai mara national reserve",
+        category: "kenya",
+        img: "",
+        desc: "` `,"
+
+    },
+    {
+        id:2,
+        title: "bwindi impenetrable national park",
+        category: "uganda",
+        img: "",
+        desc: "` `,"
+
+    },
+    {
+        id:3,
+        title: "serengeti national park",
+        category: "tanzania",
+        img: "",
+        desc: "` `,"
+
+    },
+    {
+        id:4,
+        title: "hell,s gate national park",
+        category: "kenya",
+        img: "",
+        desc: "` `,"
+
+    },
+    {
+        id:5,
+        title: "queen elizabeth national park",
+        category: "uganda",
+        img: "",
+        desc: "` `,"
+
+    },
+    {
+        id:6,
+        title: "ngorongoro crater",
+        category: "tanzania",
+        img: "",
+        desc: "` `,"
+
+    },
+    {
+        id:7,
+        title: "rift valley lakes",
+        category: "kenya",
+        img: "",
+        desc: "` `,"
+
+    },
+    {
+        id:8,
+        title: "ruwenzori mountains",
+        category: "uganda",
+        img: "",
+        desc: "` `,"
+
+    },
+    {
+        id:9,
+        title: "tangarine national park",
+        category: "tanzania",
+        img: "",
+        desc: "` `,"
+
+    },
+]
