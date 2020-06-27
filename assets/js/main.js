675e258f54e09b37371d19dcb49f550e6a3f368f
jos-lab <josphinemw@gmail.com>
2020-06-27 09:15:40 +0000

    added photos to images folder and added content to main.js about items to be filtered

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,24 +16,32 @@ const places =[
         id:2,
         title: "bwindi impenetrable national park",
         category: "uganda",
-        img: "",
-        desc: "` `,"
+        img: "assets/images/places-item2.jpg",
+        desc: `Among East African forests, Bwindi has some of the richest populations of trees, small mammals, birds, 
+        reptiles, butterflies and moths. The park's diverse species may be because the forest was a refuge for species 
+        during glaciationin the pleistocene epoch. The park's forests are afromontane, a rare vegetation type of the African
+        continent, the park is important for the coservation of afromomtane fauna. The park also is inhabited by about 600 
+        individual mountain gorillas which makes up almost half of all the mountain gorillas in the world.`,
 
     },
     {
         id:3,
         title: "serengeti national park",
         category: "tanzania",
-        img: "",
-        desc: "` `,"
+        img: "assets/images/places-item3.jpg",
+        desc: `Named for the Maasai word "siringet" (which translates roughly as "the place where the land runs on forever")
+        Serengeti National Park is among the oldest game reserves in Tanzania. It is also one of the most iconic safari 
+        destinations on the African continent. Located in the North of the country, it partly adjoins the Kenyan border
+        and together with the Masai Mara National Reserve provides the backdrop for ythe annual Grat Migration - considered
+        by many to be the planet's most impressive natural event.`,
 
     },
     {
         id:4,
-        title: "hell,s gate national park",
+        title: "hell's gate national park",
         category: "kenya",
         img: "",
-        desc: "` `,"
+        desc: ` `,
 
     },
     {
