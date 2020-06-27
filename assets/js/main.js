86e944d87806581c7128300c1456241a00b90071
jos-lab <josphinemw@gmail.com>
2020-06-27 07:26:36 +0000

    adding content to top places filter section in the places section

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,13 @@ const places =[
         id:1,
         title: "maasai mara national reserve",
         category: "kenya",
-        img: "",
-        desc: "` `,"
-
+        img: "assets/images/places-item1.jpg",
+        desc: `Maasai Mara National Reserve is one of the key popular tourism points in Kenya. It is located in 
+        South-Western Kenya along the Great Rift Valley adjacent to Serengeti National Park of Tanzania. The 
+        attraction in Maasai Mara National Reserve are both nature and wildlife. The Wildebeest migration is 
+        also witnessed in the Kenya iconic Masai Mara National Reserve. A good short safari combination for those hoping
+        to also see all these charismatic creatures (THE BIG FIVE)(and much more besides would be Masai Mara (for lion,
+        leopard,buffalo and elephant) and lake Nakuru (for black and white rhino)`,
     },
     {
         id:2,
