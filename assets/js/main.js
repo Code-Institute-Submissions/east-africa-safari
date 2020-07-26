c1f9bb30b2ca7853bcf4d114079b066bfb0e94a5
jos-lab <josphinemw@gmail.com>
2020-07-26 15:09:24 +0000

    added javascript functionality to tab in destinations secion

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,3 +144,26 @@ const places =[
     //  displayPlaces = displayPlaces.join("");
     //  sectionCenter.innerHTML = displayPlaces;
 //  });
+
+var content1 = document.getElementById("content1");
+var content2 = document.getElementById("content2");
+var content3 = document.getElementById("content3");
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+
+function openKenya() {
+    content1.style.transform="translateX(0)";
+    content2.style.transform="translateX(100%)";
+    content3.style.transform="translateX(100%)";
+}
+function openUganda() {
+    content1.style.transform="translateX(100%)";
+    content2.style.transform="translateX(0)";
+    content3.style.transform="translateX(100%)";
+}
+function openTanzania() {
+    content1.style.transform="translateX(100%)";
+    content2.style.transform="translateX(100%)";
+    content3.style.transform="translateX(0)";
+}
