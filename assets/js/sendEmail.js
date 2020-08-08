b9ae8ce9a3f408a104282e107a582947d4fc7e6f
jos-lab <josphinemw@gmail.com>
2020-08-08 22:45:35 +0000

    adjusting the carousel caption margin.and font size

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,28 @@
-function sendMail(contactForm) {
-    emailjs.send("gmail", "josephine", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "my-feedback": contactForm.feedbacksummary.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
-}
+//  function sendMail(contactForm) {
+//      emailjs.send("gmail", "josephine", {
+//          "from_name": contactForm.name.value,
+//          "from_email": contactForm.emailaddress.value,
+//          "my_feedback": contactForm.message.value
+//      })
+//      then(
+//          function(response) {
+//              console.log("SUCCESS", response);
+//          },
+//          function(error) {
+//              console.log("FAILED", error);
+//          });
+//  }
+
+ function sendMail(contactForm) {
+
+ emailjs.send("gmail", "josephine", {
+ "from_name": contactForm.name.value,
+ "from_email": contactForm.emailaddress.value,
+ "my_feedback": contactForm.message.value
+ })
+ .then((response) => {
+ console.log("SUCCESS", response);
+ }).catch((error) => {
+ console.log("FAILED", error);
+ });
+ }
